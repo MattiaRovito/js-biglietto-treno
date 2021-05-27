3229dcdeebbefd617422a3d45a5a154c3047b3db
MattiaR <rovitomattia91@gmail.com>
2021-05-27 14:49:00 +0200

    correzione (rivedere la maestra d'italiano)

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ console.log("prova");
 var km = prompt("Quanti chilometri vuoi fare?")
 console.log(km);
 
-var eta = parseInt(prompt("Quanti hanni hai?"));
+var eta = parseInt(prompt("Quanti anni hai?"));
 console.log(eta);
 
 // 2 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
 
-
+var prezzo = 0.21 * km
+console.log(prezzo)
 
 
 // 3 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
